54b4eba52afb510dea5dbb11498a5cca2d64f3a6
aidakoosha <s2184935@ed.ac.uk>
2025-11-12 10:44:26 +0000

    Fix remaining print statements in corpus_util.py - Final Python 3 compatibility

--- deep_disfluency/corpus/corpus_util.py
+++ deep_disfluency/corpus/corpus_util.py
@@ -79,11 +79,11 @@ def detection_corpus_format(uttRef, words, pos, tags, indices):
             tags[i] = "<f/>"
     final_string = "\t".join(
         [uttRef, indices.pop(0), words.pop(0), pos.pop(0), tags.pop(0)]) + "\n"
-    print len(indices), len(words), len(pos), len(tags)
-    print indices
-    print words
-    print pos
-    print tags
+    print(len(indices), len(words), len(pos), len(tags))
+    print(indices)
+    print(words)
+    print(pos)
+    print(tags)
     for i in range(0, len(tags)):
         final_string += "\t".join(["", indices[i],
                                    words[i], pos[i], tags[i]]) + "\n"
@@ -414,7 +414,7 @@ def find_repair_end_in_previous_utts(repair, overallTagList, uttlist):
     while not repair.complete == True:
         search += 1
         if search >= len(overallTagList):
-            print 'Repair not found!'
+            print('Repair not found!')
             raise Exception
         # print "search " + str(search)
         # get the tag list *search* utterances back
